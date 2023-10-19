38724afb0701e66fd42163ccd0b9f3aa3723d09d
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-19 20:15:54 +0200

    Added lights to thrusters

--- Assets/Scripts/Ship/ShipEngine.cs
+++ Assets/Scripts/Ship/ShipEngine.cs
@@ -17,17 +17,22 @@ namespace Ship
         [FormerlySerializedAs("_currentThrust")]
         [Header("Current Engine Values")] 
         [SerializeField] [Range(-1, 1)] private float _currentThrustInput = 0f;
-        [FormerlySerializedAs("_currentPitch")] [SerializeField] [Range(-1, 1)] private float _currentPitchInput = 0f;
-        [FormerlySerializedAs("_currentYaw")] [SerializeField] [Range(-1, 1)] private float _currentYawInput = 0f;
-        [FormerlySerializedAs("_currentRoll")] [SerializeField] [Range(-1, 1)] private float _currentRollInput = 0f;
+        [SerializeField] [Range(-1, 1)] private float _currentPitchInput = 0f;
+        [SerializeField] [Range(-1, 1)] private float _currentYawInput = 0f;
+        [SerializeField] [Range(-1, 1)] private float _currentRollInput = 0f;
 
         [Header("Thrusters")]
         [SerializeField] private ParticleSystem _leftBackThruster;
         [SerializeField] private ParticleSystem _rightBackThruster;
         [SerializeField] private ParticleSystem _leftFrontThruster;
         [SerializeField] private ParticleSystem _rightFrontThruster;
+        [SerializeField] private Light _leftBackThrusterLight;
+        [SerializeField] private Light _rightBackThrusterLight;
+        [SerializeField] private Light _leftFrontThrusterLight;
+        [SerializeField] private Light _rightFrontThrusterLight;
         [SerializeField] private float _engineThrustEmissionRateMax = 25.0f;
         [SerializeField] private float _engineThrustEmissionLifeTimeMax = 3.5f;
+        [SerializeField] private float _engineLightIntensityMax = 10.0f;
     
         private Vector3 _velocity;
         private Vector3 _angularVelocity; // x = pitch, y = yaw, z = roll
@@ -127,31 +132,49 @@ namespace Ship
             // emissionLifeTime += Mathf.Lerp(0, _engineThrustEmissionLifeTimeMax, Mathf.Abs(_currentYaw));
 
             // Calculate the emission rate and lifetime based on the current thrust
-            var emissionRate = Mathf.Lerp(0, _engineThrustEmissionRateMax, Mathf.Abs(_currentThrustInput)); // Use the absolute value of _currentThrust
-            var emissionLifeTime = Mathf.Lerp(0, _engineThrustEmissionLifeTimeMax, Mathf.Abs(_currentThrustInput));
+            float emissionRate = Mathf.Lerp(0, _engineThrustEmissionRateMax, Mathf.Abs(_currentThrustInput)); // Use the absolute value of _currentThrust
+            float emissionLifeTime = Mathf.Lerp(0, _engineThrustEmissionLifeTimeMax, Mathf.Abs(_currentThrustInput));
+            float emissionIntensity = Mathf.Lerp(0, _engineLightIntensityMax, Mathf.Abs(_currentThrustInput));
 
             // Determine the thrust direction (1 for positive, -1 for negative)
-            var thrustDirection = Mathf.Sign(_currentThrustInput);
+            float thrustDirection = Mathf.Sign(_currentThrustInput);
         
             // Calculate yaw emission rate and lifetime
-            var yawEmissionRate = Mathf.Lerp(0, _engineThrustEmissionRateMax, Mathf.Abs(_currentYawInput));
-            var yawEmissionLifeTime = Mathf.Lerp(0, _engineThrustEmissionLifeTimeMax, Mathf.Abs(_currentYawInput));
+            float yawEmissionRate = Mathf.Lerp(0, _engineThrustEmissionRateMax, Mathf.Abs(_currentYawInput));
+            float yawEmissionLifeTime = Mathf.Lerp(0, _engineThrustEmissionLifeTimeMax, Mathf.Abs(_currentYawInput));
+            float yawEmissionIntensity = Mathf.Lerp(0, _engineLightIntensityMax, Mathf.Abs(_currentYawInput));
         
             // yawDirection is 1 if _currentYaw is positive, -1 if _currentYaw is negative
-            var yawDirection = Mathf.Sign(_currentYawInput);
+            float yawDirection = Mathf.Sign(_currentYawInput);
 
             // Update the particle systems
+            ParticleSystem.MainModule mainModule;
+            ParticleSystem.EmissionModule emissionModule;
             // For the right thrusters
-            _rightFrontThruster.emissionRate = Mathf.Clamp(emissionRate * -thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * yawDirection, 0, _engineThrustEmissionRateMax * 0.3f); 
-            _rightFrontThruster.startLifetime = Mathf.Clamp(emissionLifeTime * -thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f); 
-            _rightBackThruster.emissionRate = Mathf.Clamp(emissionRate * thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * -yawDirection, 0, _engineThrustEmissionRateMax * 0.3f);
-            _rightBackThruster.startLifetime = Mathf.Clamp(emissionLifeTime * thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * -yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f);
-
+            mainModule = _rightFrontThruster.main;
+            emissionModule = _rightFrontThruster.emission;
+            emissionModule.rateOverTime = Mathf.Clamp(emissionRate * -thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * yawDirection, 0, _engineThrustEmissionRateMax * 0.3f); 
+            mainModule.startLifetime = Mathf.Clamp(emissionLifeTime * -thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f); 
+            _rightFrontThrusterLight.intensity = Mathf.Clamp(emissionIntensity * -thrustDirection, 0, _engineLightIntensityMax * 0.7f) + Mathf.Clamp(yawEmissionIntensity * yawDirection, 0, _engineLightIntensityMax * 0.3f);
+            
+            mainModule = _rightBackThruster.main;
+            emissionModule = _rightBackThruster.emission;
+            emissionModule.rateOverTime = Mathf.Clamp(emissionRate * thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * -yawDirection, 0, _engineThrustEmissionRateMax * 0.3f);
+            mainModule.startLifetime = Mathf.Clamp(emissionLifeTime * thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * -yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f);
+            _rightBackThrusterLight.intensity = Mathf.Clamp(emissionIntensity * thrustDirection, 0, _engineLightIntensityMax * 0.7f) + Mathf.Clamp(yawEmissionIntensity * -yawDirection, 0, _engineLightIntensityMax * 0.3f);
+            
             // For the left thrusters
-            _leftFrontThruster.emissionRate = Mathf.Clamp(emissionRate * -thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * -yawDirection, 0, _engineThrustEmissionRateMax * 0.3f); 
-            _leftFrontThruster.startLifetime = Mathf.Clamp(emissionLifeTime * -thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * -yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f); 
-            _leftBackThruster.emissionRate = Mathf.Clamp(emissionRate * thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * yawDirection, 0, _engineThrustEmissionRateMax * 0.3f);
-            _leftBackThruster.startLifetime = Mathf.Clamp(emissionLifeTime * thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f);
+            mainModule = _leftFrontThruster.main;
+            emissionModule = _leftFrontThruster.emission;
+            emissionModule.rateOverTime = Mathf.Clamp(emissionRate * -thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * -yawDirection, 0, _engineThrustEmissionRateMax * 0.3f); 
+            mainModule.startLifetime = Mathf.Clamp(emissionLifeTime * -thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * -yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f);
+            _leftFrontThrusterLight.intensity = Mathf.Clamp(emissionIntensity * -thrustDirection, 0, _engineLightIntensityMax * 0.7f) + Mathf.Clamp(yawEmissionIntensity * -yawDirection, 0, _engineLightIntensityMax * 0.3f);
+            
+            mainModule = _leftBackThruster.main;
+            emissionModule = _leftBackThruster.emission;
+            emissionModule.rateOverTime = Mathf.Clamp(emissionRate * thrustDirection, 0, _engineThrustEmissionRateMax * 0.7f) + Mathf.Clamp(yawEmissionRate * yawDirection, 0, _engineThrustEmissionRateMax * 0.3f);
+            mainModule.startLifetime = Mathf.Clamp(emissionLifeTime * thrustDirection, 0, _engineThrustEmissionLifeTimeMax * 0.7f) + Mathf.Clamp(yawEmissionLifeTime * yawDirection, 0, _engineThrustEmissionLifeTimeMax * 0.3f);
+            _leftBackThrusterLight.intensity = Mathf.Clamp(emissionIntensity * thrustDirection, 0, _engineLightIntensityMax * 0.7f) + Mathf.Clamp(yawEmissionIntensity * yawDirection, 0, _engineLightIntensityMax * 0.3f);
 
         }
     
