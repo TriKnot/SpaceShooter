f53af8fb091f346ac2710296a63daf119602f62c
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-19 17:01:52 +0200

    Improve spawner to take camera into consideration and try to spawn outside camera view.

--- Assets/Scripts/Asteroids/AsteroidSpawner.cs
+++ Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -10,10 +10,17 @@ namespace Asteroids
         [SerializeField] private float _maxspawnRadius;
         [SerializeField] private float _minSpawnDistance;
         [SerializeField] private float _spawnRate;
+        [SerializeField] private Camera _camera;
         
 
         private void Start()
         {
+            if (_camera == null)
+            {
+                Debug.LogError($"Main Camera is not assigned. Please assign the camera in the Inspector of {gameObject.name}.");
+                return;
+            }
+            
             for (int i = 0; i < _initialAsteroidCount; i++)
             {
                 SpawnAsteroid();
@@ -32,8 +39,25 @@ namespace Asteroids
 
         private void SpawnAsteroid()
         {
-            int randAsteroidIndex = Random.Range(0, _asteroidPrefabs.Length); 
-            Asteroid asteroid = Instantiate(_asteroidPrefabs[randAsteroidIndex], Random.insideUnitSphere * Random.Range(_minSpawnDistance, _maxspawnRadius), Random.rotation);
+            int randAsteroidIndex = Random.Range(0, _asteroidPrefabs.Length);
+            Vector3 spawnPos;
+            int attempts = 0; // safety measure to prevent infinite loop
+            do
+            {
+                spawnPos = Random.insideUnitSphere * Random.Range(_minSpawnDistance, _maxspawnRadius);
+            } while (!IsPositionOnScreen(spawnPos) && attempts++ < 100);
+            Asteroid asteroid = Instantiate(_asteroidPrefabs[randAsteroidIndex], spawnPos, Random.rotation);
+        }
+        
+        private bool IsPositionOnScreen(Vector3 worldPosition)
+        {
+            // Convert the world position to screen space
+            Vector3 screenPosition = _camera.WorldToViewportPoint(worldPosition);
+
+            // Check if the screen position is within the view
+            return screenPosition.x is > 0 and < 1 &&
+                   screenPosition.y is > 0 and < 1 &&
+                   screenPosition.z > 0;
         }
     }
 }
