835c9da0b938cd84eddaa8191cad0479f82129b2
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-19 18:08:21 +0200

    Make it possible to spawn huge asteroids but make it very unlikely

--- Assets/Scripts/Asteroids/Asteroid.cs
+++ Assets/Scripts/Asteroids/Asteroid.cs
@@ -11,13 +11,15 @@ namespace Asteroids
         
         [SerializeField] private float _minScaleMultiplier;
         [SerializeField] private float _maxScaleMultiplier;
+        [SerializeField] private AnimationCurve _massCurve;
         
         private float _scaleMultiplier;
         private AsteroidMovement _asteroidMovement;
 
         private void Awake()
         {
-            _scaleMultiplier = Random.Range(_minScaleMultiplier, _maxScaleMultiplier);
+            float mass = _massCurve.Evaluate(Random.value);
+            _scaleMultiplier = Mathf.Lerp(_minScaleMultiplier, _maxScaleMultiplier, mass);
             transform.localScale *= _scaleMultiplier;
             _asteroidMovement = gameObject.AddComponent<AsteroidMovement>();
             _asteroidMovement.Init(_scaleMultiplier, Mathf.Pow(_scaleMultiplier, 3));
