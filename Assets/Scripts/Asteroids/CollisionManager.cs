8435319e17568582cfeaec50edefe40cfb1000b1
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-19 16:31:22 +0200

    Improved collision calculation to be more accurate.

--- Assets/Scripts/Asteroids/CollisionManager.cs
+++ Assets/Scripts/Asteroids/CollisionManager.cs
@@ -13,20 +13,12 @@ namespace Asteroids
             Vector3 initialVelocity1 = asteroid1.Velocity;
             Vector3 initialVelocity2 = asteroid2.Velocity;
 
-            // Calculate the initial total momentum
-            Vector3 initialTotalMomentum = mass1 * initialVelocity1 + mass2 * initialVelocity2;
-
-            // Calculate the relative velocity
-            Vector3 relativeVelocity = initialVelocity1 - initialVelocity2;
-
             // Calculate the final velocities using the equations for elastic collision
-            Vector3 finalVelocity1 = initialVelocity1 - (2 * mass2 / (mass1 + mass2)) * (Vector3.Dot(relativeVelocity, initialVelocity1 - initialVelocity2) / Vector3.Dot(initialVelocity1 - initialVelocity2, initialVelocity1 - initialVelocity2)) * (initialVelocity1 - initialVelocity2);
-            Vector3 finalVelocity2 = initialVelocity2 - (2 * mass1 / (mass1 + mass2)) * (Vector3.Dot(relativeVelocity, initialVelocity2 - initialVelocity1) / Vector3.Dot(initialVelocity2 - initialVelocity1, initialVelocity2 - initialVelocity1)) * (initialVelocity2 - initialVelocity1);
+            Vector3 finalVelocity1 = (initialVelocity1 * (mass1 - mass2) + 2 * mass2 * initialVelocity2) / (mass1 + mass2);
+            Vector3 finalVelocity2 = (initialVelocity2 * (mass2 - mass1) + 2 * mass1 * initialVelocity1) / (mass1 + mass2);
 
             asteroid1.Velocity = finalVelocity1;
             asteroid2.Velocity = finalVelocity2;
-            
-            Debug.Log($"Initial Total Momentum: {initialTotalMomentum} -->> Final Total Momentum: {mass1 * finalVelocity1 + mass2 * finalVelocity2}");
 
         }
     }
