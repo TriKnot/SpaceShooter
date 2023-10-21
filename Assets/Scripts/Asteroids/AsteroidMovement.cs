68f75f1ff62474bdde4d29f7accae1887526ff80
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-21 20:03:52 +0200

    Improved collision handling

--- Assets/Scripts/Asteroids/AsteroidMovement.cs
+++ Assets/Scripts/Asteroids/AsteroidMovement.cs
@@ -17,7 +17,13 @@ namespace Asteroids
             get => _velocity;
             set => _velocity = value;
         }
-        public Vector3 AngularVelocity => _angularVelocity;
+
+        public Vector3 AngularVelocity
+        {
+            get => _angularVelocity;
+            set => _angularVelocity = value;
+        }
+        
         public float Mass => _mass;
 
 
@@ -25,9 +31,9 @@ namespace Asteroids
         {
             _velocity = Random.insideUnitSphere * Random.Range(0, 100) + initialVelocity;
             _angularVelocity = Random.insideUnitSphere * Random.Range(0, 100) + initialAngularVelocity;
-            _mass = mass * scaleMultiplier;
+            _mass = mass;
             
-            Invoke(nameof(EnableCollision), 1f);
+            Invoke(nameof(EnableCollision), 0.1f);
         }
 
         public void FixedUpdate()
@@ -41,7 +47,10 @@ namespace Asteroids
         {
             if (!collisionIsOn) return;
             if (!other.gameObject.TryGetComponent(out AsteroidMovement asteroid)) return;
-            CollisionManager.CalculateCollision(this, asteroid);
+            // Let the larger asteroid send the collision message
+            if (asteroid.Mass > _mass) return;
+            Vector3 hitPoint = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+            CollisionManager.CalculateCollision(this, asteroid, hitPoint);
         }
 
         private void EnableCollision()
@@ -49,5 +58,16 @@ namespace Asteroids
             collisionIsOn = true;
         }
 
+        public void AddAngularVelocity(Vector3 collisionTorque1)
+        {
+            _angularVelocity += collisionTorque1 / _mass;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Vector3 startPos = transform.position + _velocity.normalized * transform.localScale.magnitude;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(startPos, _velocity);
+        }
     }
 }
--- Assets/Scripts/Asteroids/CollisionManager.cs
+++ Assets/Scripts/Asteroids/CollisionManager.cs
@@ -5,21 +5,44 @@ namespace Asteroids
 
     public static class CollisionManager
     {
-        public static void CalculateCollision(AsteroidMovement asteroid1, AsteroidMovement asteroid2)
+        public static void CalculateCollision(AsteroidMovement asteroid1, AsteroidMovement asteroid2, Vector3 contactPoint)
         {
             // Define the masses and initial velocities of two bodies
             float mass1 = asteroid1.Mass;
             float mass2 = asteroid2.Mass;
+    
             Vector3 initialVelocity1 = asteroid1.Velocity;
             Vector3 initialVelocity2 = asteroid2.Velocity;
 
-            // Calculate the final velocities using the equations for elastic collision
-            Vector3 finalVelocity1 = (initialVelocity1 * (mass1 - mass2) + 2 * mass2 * initialVelocity2) / (mass1 + mass2);
-            Vector3 finalVelocity2 = (initialVelocity2 * (mass2 - mass1) + 2 * mass1 * initialVelocity1) / (mass1 + mass2);
+            // Calculate the relative velocity
+            Vector3 relativeVelocity = initialVelocity1 - initialVelocity2;
 
+            // Calculate the collision normal (direction along which the collision occurs)
+            Vector3 collisionNormal = (contactPoint - asteroid1.transform.position).normalized;
+
+            // Calculate the change in linear velocity (reflect off the collision plane)
+            float relativeSpeed = Vector3.Dot(relativeVelocity, collisionNormal);
+            Vector3 linearVelocityChange = -2 * relativeSpeed * collisionNormal;
+
+            Vector3 finalVelocity1 = initialVelocity1 + (linearVelocityChange * (mass2 / (mass1 + mass2)));
+            Vector3 finalVelocity2 = initialVelocity2 - (linearVelocityChange * (mass1 / (mass1 + mass2)));
+
+            // // Calculate the change in angular velocity (collision torque)
+            // Vector3 relativePosition1 = contactPoint - asteroid1.transform.position;
+            // Vector3 relativePosition2 = contactPoint - asteroid2.transform.position;
+            //
+            // Vector3 collisionTorque1 = Vector3.Cross(relativePosition1, linearVelocityChange * mass2);
+            // Vector3 collisionTorque2 = Vector3.Cross(relativePosition2, linearVelocityChange * mass1);
+            //
+            // // Add velocity based on the collision torque
+            // finalVelocity1 += collisionTorque1 / mass1;
+            // finalVelocity2 += collisionTorque2 / mass2;
+            
             asteroid1.Velocity = finalVelocity1;
             asteroid2.Velocity = finalVelocity2;
 
         }
+
+
     }
 }
