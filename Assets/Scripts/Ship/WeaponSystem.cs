04632869a188e8572fc45d1e69d0ff02c26c0bba
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-21 17:14:05 +0200

    Create buffer for laser shot

--- Assets/Scripts/Ship/WeaponSystem.cs
+++ Assets/Scripts/Ship/WeaponSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Ship
@@ -7,12 +8,46 @@ namespace Ship
         [SerializeField] private Laser _laserPrefab;
         [SerializeField] private Transform _laserSpawnPoint;
         [SerializeField] private Vector3 _laserStartRotation;
+        [SerializeField] private float cooldownTime = 0.5f;
 
-        public void ShootLaser()
+        private bool _isOnCooldown = false;
+        private float _lastShotTime;
+        private bool _bufferShot = false;
+        
+        private void Update()
+        {
+            // Check if the cooldown is over
+            if (_isOnCooldown && Time.time - _lastShotTime > cooldownTime)
+            {
+                _isOnCooldown = false;
+            }
+            
+            if(_bufferShot && !_isOnCooldown){
+                ShootLaser();
+            }
+        }
+
+        private void SpawnLaser()
         {
             // Instantiate a laser
             Laser laser = Instantiate(_laserPrefab, _laserSpawnPoint.position, _laserSpawnPoint.rotation * Quaternion.Euler(_laserStartRotation));
             laser.Init(_laserSpawnPoint.forward);
+            Debug.Log("PewPew!");
+        }
+
+        public void ShootLaser()
+        {
+            if (_isOnCooldown)
+            {
+                _bufferShot = true;
+                return;
+            }
+            // Spawn a laser shot
+            SpawnLaser();
+            // Set the cooldown and update the last shot time
+            _isOnCooldown = true;
+            _lastShotTime = Time.time;
+            _bufferShot = false;
         }
 
     }
