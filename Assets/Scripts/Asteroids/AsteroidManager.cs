ae61ba0863feaa10a1d02bea20e54f61d2411842
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-25 15:35:37 +0200

    Break out spawning logic into static script

--- Assets/Scripts/Asteroids/AsteroidManager.cs
+++ Assets/Scripts/Asteroids/AsteroidManager.cs
@@ -1,27 +1,23 @@
 using System.Collections;
 using Jobs;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Jobs;
-using Random = UnityEngine.Random;
 using ScriptableObjects.Variables;
-using Unity.Burst;
-using UnityEngine.Serialization;
 
 namespace Asteroids
 {
     [BurstCompile]
     public class AsteroidManager : MonoBehaviour
     {
-        [FormerlySerializedAs("_asteroidPrefabs")]
         [Header("Setup")]
         [SerializeField] private AsteroidArraySO _asteroidPrefabsSO;
         [SerializeField] private IntVariableSO _initialAsteroidCountSO;
         [SerializeField] private FloatVariableSO _maxspawnRadiusSO;
         [SerializeField] private FloatVariableSO _minSpawnDistanceSO;
         [SerializeField] private FloatVariableSO _spawnRateSO;
-        [SerializeField] private GameObjectVariableSO _cameraGameObjectSO;
         [SerializeField] private TransformVariableSO _playerTransformSO;
         [SerializeField] private BoolVariableSO _usePoolingSO;
         [SerializeField] private AsteroidObjectPoolSO _asteroidPoolSO;
@@ -32,23 +28,20 @@ namespace Asteroids
         private TransformMoveJob _asteroidMoveJob;
         private JobHandle _asteroidMoveJobHandle;
         private TransformAccessArray _asteroidTransformAccessArray;
-        private Camera _camera;
-        
-        
+
         private void Start()
         {
             InitializeArrays();
-            SpawnStartAsteroids(_playerTransformSO.Value);
+            SpawnStartAsteroids();
             InitializeAsteroids();
-            if(_spawnRateSO.Value < 0)
+            if (_spawnRateSO.Value < 0)
                 StartCoroutine(SpawnAsteroidIEnumerator());
         }
 
         private void InitializeArrays()
         {
             _spawnedAsteroids = new Asteroid[_initialAsteroidCountSO.Value];
-            _asteroidMoveDataArray =
-                new NativeArray<MoveData>(_initialAsteroidCountSO.Value, Allocator.Persistent);
+            _asteroidMoveDataArray = new NativeArray<MoveData>(_initialAsteroidCountSO.Value, Allocator.Persistent);
         }
 
         private void InitializeAsteroids()
@@ -68,7 +61,7 @@ namespace Asteroids
             EnsureAsteroidJobComplete();
             ScheduleAsteroidMoveJob();
         }
-        
+
         private void ScheduleAsteroidMoveJob()
         {
             for (int i = 0; i < _initialAsteroidCountSO.Value; i++)
@@ -95,116 +88,30 @@ namespace Asteroids
             _asteroidMoveDataArray.Dispose();
             _asteroidTransformAccessArray.Dispose();
         }
-        
+
         private IEnumerator SpawnAsteroidIEnumerator()
         {
             while (true)
             {
                 yield return new WaitForSeconds(_spawnRateSO.Value);
-                Vector3 centerPosOffset = GetCenterPosOffset();
-                SpawnAsteroid(centerPosOffset);
+                SpawnAsteroid();
             }
         }
 
-        private Vector3 GetCenterPosOffset()
+        private void SpawnStartAsteroids()
         {
-            return _playerTransformSO.Value ? _playerTransformSO.Value.position : Vector3.zero;
-        }
-
-        private void SpawnStartAsteroids(Transform playerTransform)
-        {
-            Vector3 centerPosOffset = GetCenterPosOffset();
-
             for (int i = 0; i < _initialAsteroidCountSO.Value; i++)
             {
-                Asteroid asteroid = SpawnAsteroid(centerPosOffset);
+                Asteroid asteroid = SpawnAsteroid();
                 _spawnedAsteroids[i] = asteroid;
                 _asteroidMoveDataArray[i] = asteroid.AsteroidMoveData;
             }
         }
 
-        private Asteroid SpawnAsteroid(Vector3 centerPosOffset = default)
+        private Asteroid SpawnAsteroid()
         {
-            Vector3 spawnPos = GetRandomSpawnPosition(centerPosOffset);
-            Asteroid asteroid = _usePoolingSO.Value ? SpawnAsteroidFromPool() : SpawnAsteroidFromPrefab();
-            asteroid.Init();
-            asteroid.Activate(spawnPos, Random.rotation);
-            return asteroid;
-        }
-
-        private Vector3 GetRandomSpawnPosition(Vector3 centerPosOffset)
-        {
-            Vector3 spawnPos;
-            int attempts = 0; // Safety measure to prevent infinite loop
-
-            do
-            {
-                spawnPos = Random.insideUnitSphere * Random.Range(_minSpawnDistanceSO.Value, _maxspawnRadiusSO.Value) + centerPosOffset;
-            } while (PointInCameraView(spawnPos) && attempts++ < 100);
-
-            return spawnPos;
-        }
-
-        private Asteroid SpawnAsteroidFromPrefab()
-        {
-            int randAsteroidIndex = Random.Range(0, _asteroidPrefabsSO.Value.Length);
-            Asteroid asteroid = Instantiate(_asteroidPrefabsSO.Value[randAsteroidIndex]);
-            asteroid.gameObject.SetActive(false);
-            return asteroid;
-        }
-
-        private Asteroid SpawnAsteroidFromPool()
-        {
-            return _asteroidPoolSO.Value.Get();
-        }
-
-        private bool PointInCameraView(Vector3 point)
-        {
-            if (!EnsureCameraComponent())
-            {
-                return false;
-            }
-
-            Vector3 viewport = _camera.WorldToViewportPoint(point);
-            bool inCameraFrustum = Is01(viewport.x) && Is01(viewport.y);
-            bool inFrontOfCamera = viewport.z > 0;
-            bool objectBlockingPoint = CheckObjectBlockingPoint(point);
-
-            return inCameraFrustum && inFrontOfCamera && !objectBlockingPoint;
-        }
-
-        private bool EnsureCameraComponent()
-        {
-            if (_camera == null && _cameraGameObjectSO.Value != null)
-            {
-                _camera = _cameraGameObjectSO.Value.GetComponent<Camera>();
-            }
-
-            return _camera != null;
-        }
-
-        private bool CheckObjectBlockingPoint(Vector3 point)
-        {
-            RaycastHit depthCheck;
-            bool objectBlockingPoint = false;
-            Vector3 directionBetween = (point - _camera.transform.position).normalized;
-            float distance = Vector3.Distance(_camera.transform.position, point);
-
-            if (Physics.Raycast(_camera.transform.position, directionBetween, out depthCheck, distance + 0.05f))
-            {
-                if (depthCheck.point != point)
-                {
-                    objectBlockingPoint = true;
-                }
-            }
-
-            return objectBlockingPoint;
-        }
-
-        private static bool Is01(float a)
-        {
-            return a > 0 && a < 1;
+            return AsteroidSpawner.SpawnAsteroid(
+                _asteroidPrefabsSO.Value, _minSpawnDistanceSO.Value, _maxspawnRadiusSO.Value, _usePoolingSO, _playerTransformSO.Value,_asteroidPoolSO.Value);
         }
     }
-    
 }
---
+++ Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Util;
+
+namespace Asteroids
+{
+    [BurstCompile]
+    public class AsteroidSpawner
+    {
+        public static Asteroid SpawnAsteroid(
+            Asteroid[] asteroidPrefabs,
+            float minSpawnDistance,
+            float maxSpawnRadius,
+            bool usePooling,
+            Transform playerTransform = null,
+            ObjectPool<Asteroid> asteroidPool = null)
+        {
+            Vector3 centerPosOffset = playerTransform ? playerTransform.position : Vector3.zero;
+            Vector3 spawnPos = GetRandomSpawnPosition(centerPosOffset, minSpawnDistance, maxSpawnRadius);
+
+            Asteroid asteroid = usePooling || asteroidPool == null ? SpawnAsteroidFromPool(asteroidPool) : SpawnAsteroidFromPrefab(asteroidPrefabs);
+            asteroid.Init();
+            asteroid.Activate(spawnPos, Random.rotation);
+            return asteroid;
+        }
+
+        private static Vector3 GetRandomSpawnPosition(Vector3 centerPosOffset, float minSpawnDistance, float maxSpawnRadius)
+        {
+            Vector3 spawnPos;
+            Camera mainCamera = Camera.main; // Kind of a hack for the time being
+            int attempts = 0; // Safety measure to prevent infinite loop
+
+            do
+            {
+                spawnPos = Random.insideUnitSphere * Random.Range(minSpawnDistance, maxSpawnRadius) + centerPosOffset;
+            } while (PointInCameraView(spawnPos, mainCamera) && attempts++ < 100);
+
+            return spawnPos;
+        }
+
+        private static Asteroid SpawnAsteroidFromPrefab(Asteroid[] asteroidPrefabs)
+        {
+            int randAsteroidIndex = Random.Range(0, asteroidPrefabs.Length);
+            Asteroid asteroid = Object.Instantiate(asteroidPrefabs[randAsteroidIndex]);
+            asteroid.gameObject.SetActive(false);
+            return asteroid;
+        }
+
+        private static Asteroid SpawnAsteroidFromPool(ObjectPool<Asteroid> asteroidPool)
+        {
+            return asteroidPool.Get();
+        }
+
+        private static bool PointInCameraView(Vector3 point, Camera camera)
+        {
+            if (camera == null)
+            {
+                return false;
+            }
+
+            Vector3 viewport = camera.WorldToViewportPoint(point);
+            bool inCameraFrustum = Is01(viewport.x) && Is01(viewport.y);
+            bool inFrontOfCamera = viewport.z > 0;
+            bool objectBlockingPoint = CheckObjectBlockingPoint(point, camera.transform.position);
+
+            return inCameraFrustum && inFrontOfCamera && !objectBlockingPoint;
+        }
+
+        private static bool Is01(float a)
+        {
+            return a is > 0 and < 1;
+        }
+
+        private static bool CheckObjectBlockingPoint(Vector3 point, Vector3 cameraPosition)
+        {
+            bool objectBlockingPoint = false;
+            Vector3 directionBetween = (point - cameraPosition).normalized;
+            float distance = Vector3.Distance(cameraPosition, point);
+
+            if (Physics.Raycast(cameraPosition, directionBetween, out var depthCheck, distance + 0.05f))
+            {
+                if (depthCheck.point != point)
+                {
+                    objectBlockingPoint = true;
+                }
+            }
+
+            return objectBlockingPoint;
+        }
+    }
+}
