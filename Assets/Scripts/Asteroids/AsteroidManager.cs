86becdacb28fcb11c789865cf8c867006030ae4b
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-27 17:32:54 +0200

    Ensure Job is complete before updating move data arrays

--- Assets/Scripts/Asteroids/AsteroidManager.cs
+++ Assets/Scripts/Asteroids/AsteroidManager.cs
@@ -112,6 +112,8 @@ namespace Asteroids
             
             if(MoveDataHasChanged)
             {
+                EnsureJobComplete(_asteroidMoveJobHandle);
+                EnsureJobComplete(_asteroidPieceMoveJobHandle);
                 UpdateMoveDataArrays();
             }
             
