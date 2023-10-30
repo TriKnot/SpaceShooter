4592d69f0f2611366bea469d4e992f65537d7167
TriKnot <90341216+TriKnot@users.noreply.github.com>
2023-10-30 15:11:09 +0100

    Create AsteroidData struct

---
+++ Assets/Scripts/Jobs/AsteroidData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Jobs
+{
+    public class AsteroidData
+    {
+        public float ScaleMultiplier;
+        public float Mass;
+        public bool IsPiece;
+        
+        public AsteroidData(float scaleMultiplier, bool isPiece = false)
+        {
+            ScaleMultiplier = scaleMultiplier;
+            Mass = Mathf.Pow(ScaleMultiplier, 3);
+            IsPiece = isPiece;
+        }
+        
+    }
+}
